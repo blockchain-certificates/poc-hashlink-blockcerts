ead8e12063f4add37ca68578419619058e782fd2
Julien Fraichot <julien.fraichot@hyland.com>
2022-02-28 13:58:30 -0600

    create file with header if it does not exist

--- server/logToFile.ts
+++ server/logToFile.ts
@@ -1,12 +1,31 @@
 import fs from 'fs';
 import {LogEventTimeObject} from "../bindingContext/events";
 
-export default async function logToFile (data: LogEventTimeObject) {
+const filePath = `${__dirname}/logs/times.csv`;
+const csvHeader = 'event,imageIndex,imageHashlink,timeFromStart';
+
+async function writeData (data: LogEventTimeObject) {
   const writeData = '\n' + Object.values(data).join(',');
   console.log('log to file', writeData);
-  await fs.writeFile(`${__dirname}/logs/times.csv`, writeData,{ flag: 'a' }, (error) => {
+  await fs.writeFile(filePath, writeData,{ flag: 'a' }, (error) => {
     if (error) {
       console.error(error);
     }
+  });
+}
+
+export default async function logToFile (data: LogEventTimeObject) {
+  await fs.readFile(filePath, async function (error) {
+    if (error) {
+      console.log('file does not exist, creating');
+      await fs.writeFile(filePath, csvHeader, null, function (error) {
+        if (error) {
+          console.error(error);
+        }
+        writeData(data);
+      });
+      return;
+    }
+    writeData(data);
   });
 }
