0c42a88d7adcc2a8a6acd617a71d951f5f8f2c8f
Julien Fraichot <julien.fraichot@hyland.com>
2022-02-28 13:32:45 -0600

    log to file

--- server/index.ts
+++ server/index.ts
@@ -2,13 +2,24 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import {loggingPath, port} from "../bindingContext/endpoint";
+import {LogEventTimeObject} from "../bindingContext/events";
+import logToFile from "./logToFile";
 
 const server = express();
 server.use(bodyParser.json({limit: '1mb'}));
 server.use(cors());
 
-server.post(loggingPath, async (req, res) => {
+server.post<LogEventTimeObject>(loggingPath, async (req, res) => {
   console.log(req.body);
+  try {
+    await logToFile(req.body);
+  } catch (e) {
+    res.send({
+      status: 500,
+      message: e
+    })
+    throw new Error(e);
+  }
   res.send('ok');
 });
 
